e78b0bc0b0866f12af36791f8ecf0c7f8a6c7221
Eduardo Gorio <goriofab@gmail.com>
2026-09-17 09:50:54 -0300

    feat(yondcast): expand dock into native control center

--- plugins/yondcast/yondcast-dock.hpp
+++ plugins/yondcast/yondcast-dock.hpp
@@ -4,6 +4,7 @@
 
 class QLabel;
 class QPushButton;
+class QStackedWidget;
 
 class YondCastDock final : public QWidget {
 	Q_OBJECT
@@ -16,9 +17,33 @@ public slots:
 	void setRecordingActive(bool active);
 	void refreshStatus();
 
+private slots:
+	void toggleStreaming();
+	void toggleRecording();
+	void toggleStudioMode();
+	void showProducePage();
+	void showEngagePage();
+	void showEarnPage();
+
 private:
+	void updateQuickActions();
+	void setPage(int index);
+
 	QLabel *engineStatusLabel = nullptr;
 	QLabel *streamingStatusLabel = nullptr;
 	QLabel *recordingStatusLabel = nullptr;
+	QLabel *profileLabel = nullptr;
+	QLabel *collectionLabel = nullptr;
+	QLabel *sceneLabel = nullptr;
+	QLabel *studioModeLabel = nullptr;
+
+	QPushButton *streamButton = nullptr;
+	QPushButton *recordButton = nullptr;
+	QPushButton *studioModeButton = nullptr;
+	QPushButton *produceButton = nullptr;
+	QPushButton *engageButton = nullptr;
+	QPushButton *earnButton = nullptr;
 	QPushButton *refreshButton = nullptr;
+
+	QStackedWidget *pages = nullptr;
 };
